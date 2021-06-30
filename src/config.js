40897e69442485ada523bb6c6cd641fe4d8a59d0
MaartenGDev <maarten.grootoonk@gmail.com>
2021-06-30 12:28:54 +0200

    Update endpoint

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 const isDevelopmentBuild = process.env.NODE_ENV === 'development';
 
-export const API_ENDPOINT = isDevelopmentBuild ? 'http://127.0.0.1:8000/api/v1' : 'https://christmas-maarten.azurewebsites.net/api/v1';
+export const API_ENDPOINT = isDevelopmentBuild ? 'http://127.0.0.1:8000/api/v1' : 'https://christmas-api.maartendev.me/api/v1';
